21d80bd43bfb9533121ac3b33608c16fe8a85b1c
yohas <yohaseelan@outlook.com>
2021-11-09 21:01:13 +0530

    Student Create function

---
+++ database/migrations/2021_11_09_133225_create_students_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name',100);
+            $table->string('last_name',100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
---
+++ resources/views/student/create.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Create Student</title>
+</head>
+<body>
+    <form action="{{route('students.store')}}" method="post">
+        @csrf
+    <label for="fname">First Name</label>
+    <input type="text" name="fname" id="fname">
+    <br>
+    <label for="lname">Last Name</label>
+    <input type="text" name="lname" id="lname">
+    <br>
+    <input type="submit" value="Save">
+    </form>
+</body>
+</html>
